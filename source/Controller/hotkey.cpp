ea8e86ae81f4b92dad7e57be2e59d240c618e3c6
Silver <silver1867@outlook.com>
2024-07-21 10:45:44 +0800

    fixes

--- source/Controller/hotkey.cpp
+++ source/Controller/hotkey.cpp
@@ -24,12 +24,12 @@ DWORD CSOL24H::HandleHotKey(LPVOID lpParam) noexcept
     //     ConsoleLog("【错误】尝试屏蔽键盘热键失败。错误代码：%lu。", GetLastError());
     //     return -1;
     // }
-    bSuccess = bSuccess && RegisterHotKey(NULL, 0, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '0');
-    bSuccess = bSuccess && RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '1');
-    bSuccess = bSuccess && RegisterHotKey(NULL, 2, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '2');
-    bSuccess = bSuccess && RegisterHotKey(NULL, 3, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '3');
-    bSuccess = bSuccess && RegisterHotKey(NULL, 4, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '4');
-    bSuccess = bSuccess && RegisterHotKey(NULL, 5, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '5');
+    bSuccess = bSuccess && RegisterHotKey(NULL, '0', MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '0');
+    bSuccess = bSuccess && RegisterHotKey(NULL, '1', MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '1');
+    bSuccess = bSuccess && RegisterHotKey(NULL, '2', MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '2');
+    bSuccess = bSuccess && RegisterHotKey(NULL, '3', MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '3');
+    bSuccess = bSuccess && RegisterHotKey(NULL, '4', MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '4');
+    bSuccess = bSuccess && RegisterHotKey(NULL, '5', MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, '5');
     // bSuccess = bSuccess && RegisterHotKey(NULL, 6, MOD_WIN | MOD_SHIFT | MOD_NOREPEAT, 'T'); /* GamingTool 功能隐藏任务栏 */
     // bSuccess = bSuccess && RegisterHotKey(NULL, 7, MOD_CONTROL | MOD_ALT | MOD_NOREPEAT, 'B'); /* GamingTool 功能 Ctrl Alt B */
     // bSuccess = bSuccess && RegisterHotKey(NULL, 8, MOD_CONTROL | MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, 'B'); /* GamingTool 功能 Ctrl Alt Shift B */
@@ -45,46 +45,44 @@ DWORD CSOL24H::HandleHotKey(LPVOID lpParam) noexcept
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0))
     {
-        if (msg.message == WM_HOTKEY && msg.wParam >= 0 && msg.wParam <= 5)
+        if (msg.message == WM_HOTKEY && msg.wParam >= '0' && msg.wParam <= '5')
         {
             ResetEvent(hEnableWatchGameStateEvent);
             ResetEvent(hEnableWatchGameProcessEvent);
             ResetEvent(hEnableCombinePartsEvent);
             ResetEvent(hEnablePurchaseItemEvent);
             ResetEvent(hEnableLocateCursorEvent);
-            if (msg.wParam == 0)
+            if (msg.wParam == '0')
             {
                 ConsoleLog("【消息】切换为 0 模式\r\n");
                 GiveCommand(LUA_CMD_NOP); /* 写入空命令，LUA 脚本暂停运行 */
             }
-            else if (msg.wParam == 1)
+            else if (msg.wParam == '1')
             {
                 CSOL24H::DisableExtendedMode();
                 SetEvent(hEnableWatchGameStateEvent);
                 SetEvent(hEnableWatchGameProcessEvent);
                 ConsoleLog("【消息】切换为 1 模式。\r\n");
             }
-            else if (msg.wParam == 2)
+            else if (msg.wParam == '2')
             {
                 CSOL24H::EnableExtendedMode();
                 SetEvent(hEnableWatchGameStateEvent);
                 SetEvent(hEnableWatchGameProcessEvent);
                 ConsoleLog("【消息】切换为 2 模式。\r\n");
             }
-            else if (msg.wParam == 3) /* 合成配件 */
+            else if (msg.wParam == '3') /* 合成配件 */
             {
                 SetEvent(hEnableCombinePartsEvent);
-                SetEvent(hEnableWatchGameProcessEvent);
                 ConsoleLog("【消息】切换为 3 模式。\r\n");
                 GiveCommand(LUA_CMD_COMBINE_PARTS);
             }
-            else if (msg.wParam == 4) /* 购买物品 */
+            else if (msg.wParam == '4') /* 购买物品 */
             {
                 SetEvent(hEnablePurchaseItemEvent);
-                SetEvent(hEnableWatchGameProcessEvent);
                 ConsoleLog("【消息】切换为 4 模式。\r\n");
             }
-            else if (msg.wParam == 5) /* 光标定位 */
+            else if (msg.wParam == '5') /* 光标定位 */
             {
                 SetEvent(hEnableLocateCursorEvent);
                 ConsoleLog("【消息】切换为 5 模式。\r\n");
