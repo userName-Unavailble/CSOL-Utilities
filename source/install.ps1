4d68e8f22eb799464c01cd05b0b891126c7d2162
Silver <silver1867@outlook.com>
2024-07-24 16:48:52 +0800

    update

--- source/install.ps1
+++ source/install.ps1
@@ -4,4 +4,3 @@ $file = Get-Item "lua/main.lua"
 $content = (Get-Content -Encoding utf8 -Path $file.FullName) -replace("PATH\s*=\s*`".*`"", "PATH = `"$PROJECT_PATH/lua/`"")
 $utf8_no_bom = New-Object System.Text.UTF8Encoding $False
 [System.IO.File]::WriteAllLines($file.FullName, $content, $utf8_no_bom)
-Set-Location $PROJECT_PATH
