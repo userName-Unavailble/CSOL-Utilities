ab80a359f261e770b2588e3b52e485da278c6760
Silver <silver1867@outlook.com>
2024-07-26 20:03:55 +0800

    update

--- source/lua/Interpreter.lua
+++ source/lua/Interpreter.lua
@@ -9,35 +9,35 @@ local function expired()
 end
 
 function Interpreter()
-    local previous_command = Command.NOP
+    local previous_command = Command.NOP -- 上一次获取到的命令，初始为 NOP
     while (true)
     do
         Load("$~cmd.lua")
         Cmd = Cmd or Command.CMD_NOP
         -- 文件中的命令时间戳与当前时间的差值达到 5 秒认为文件中的命令无效
-        if (Cmd == Command.CMD_START_GAME_ROOM and not expired())
+        if (Cmd == Command.CMD_START_GAME_ROOM and not expired()) -- 开始游戏
         then
             Executor:start_game_room()
-        elseif (Cmd == Command.CMD_CHOOSE_CLASS and not expired())
+        elseif (Cmd == Command.CMD_CHOOSE_CLASS and not expired()) -- 选定角色
         then
             Executor:choose_class()
-        elseif (Cmd == Command.CMD_PLAY_GAME_NORMAL and not expired())
+        elseif (Cmd == Command.CMD_PLAY_GAME_NORMAL and not expired()) -- 24H 挂机模式（常规）
         then
             Executor:try_confirm()
             Player:buy_part_weapon(PartWeaponList)
             Player:play(DefaultWeaponList)
-        elseif (Cmd == Command.CMD_PLAY_GAME_EXTEND and not expired())
+        elseif (Cmd == Command.CMD_PLAY_GAME_EXTEND and not expired()) -- 24H 挂机模式（扩展）
         then
             Executor:try_confirm()
             Player:use_special_weapon(SpecialWeapon)
             Player:play(ExtendedWeaponList)
-        elseif (Cmd == Command.CMD_CREATE_ROOM and not expired())
+        elseif (Cmd == Command.CMD_CREATE_ROOM and not expired()) -- 创建房间功能
         then
             Executor:create_game_room()
-        elseif (Cmd == Command.CMD_COMBINE_PARTS and not expired())
+        elseif (Cmd == Command.CMD_COMBINE_PARTS and not expired()) -- 合成配件功能
         then
             Executor:combine_parts()
-        elseif (Cmd == Command.CMD_PURCHASE_ITEM and not expired())
+        elseif (Cmd == Command.CMD_PURCHASE_ITEM and not expired()) -- 购买物品功能
         then
             local buy_button_x = 0
             local buy_button_y = 0
@@ -46,11 +46,13 @@ function Interpreter()
                 buy_button_x, buy_button_y = Mouse:locate_cursor()
             end
             Executor:purchase_item(buy_button_x, buy_button_y)
-        elseif (Cmd == Command.CMD_LOCATE_CURSOR and not expired())
+        elseif (Cmd == Command.CMD_LOCATE_CURSOR and not expired()) -- 光标定位功能
         then
             Executor:locate_cursor()
+        else
+            Runtime:sleep(10)
         end
-        Runtime:sleep(10)
+        previous_command = Cmd -- 更新上一次命令
     end
 end
 
