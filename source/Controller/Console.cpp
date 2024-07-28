a1465c83d442a2d194d8a0a74a16064a469ba179
Silver <silver1867@outlook.com>
2024-07-29 01:42:32 +0800

    update

--- source/Controller/Console.cpp
+++ source/Controller/Console.cpp
@@ -2,10 +2,13 @@
 #include "CSOL24H.hpp"
 #include <Windows.h>
 #include <cstddef>
+#include <errhandlingapi.h>
 #include <handleapi.h>
 #include <iostream>
 #include <minwindef.h>
+#include <processenv.h>
 #include <synchapi.h>
+#include <wincon.h>
 
 HANDLE hConsoleMutex = NULL;
 
@@ -34,7 +37,7 @@ bool InitializeConsole() noexcept
     hConsoleMutex = CreateMutexW(NULL, FALSE, NULL);
     if (!hConsoleMutex)
     {
-        std::printf("【错误】创建控制台互斥体失败。错误代码 %lu。\r\n", GetLastError());
+        std::printf("【错误】创建控制台互斥体失败。错误代码：%lu。\r\n", GetLastError());
         return false;
     }
     SetConsoleCP(CP_UTF8);
@@ -43,10 +46,30 @@ bool InitializeConsole() noexcept
     std::puts("【消息】控制台代码页及区域格式设为 UTF-8。");
     if (!SetConsoleCtrlHandler(HandlerRoutine, TRUE))
     {
-        std::printf("【错误】注册控制台控制信号处理函数失败。错误代码 %lu。\r\n", GetLastError());
+        std::printf("【错误】注册控制台控制信号处理函数失败。错误代码：%lu。\r\n", GetLastError());
         return false;
     }
     std::puts("【消息】注册控制台控制信号处理函数。");
+    DWORD dwMode;
+    if (!GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &dwMode))
+    {
+        std::printf("【消息】获取控制台输入模式失败。错误代码：%lu。\r\n", GetLastError());
+        return false;
+    }
+    if (dwMode & ENABLE_QUICK_EDIT_MODE)
+    {
+        std::puts("【消息】检测到快速编辑模式开启，尝试关闭快速编辑模式。");
+        if (!SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), dwMode & ~ENABLE_QUICK_EDIT_MODE))
+        {
+            std::printf("【错误】设置控制台输入模式时出现错误。错误代码：%lu。\r\n", GetLastError());
+            return false;
+        }
+        std::puts("【消息】成功禁用控制台快速编辑模式。");
+    }
+    else
+    {
+        std::puts("【消息】检测到控制台快速编辑模式已禁用。");
+    }
     return true;
 }
 
