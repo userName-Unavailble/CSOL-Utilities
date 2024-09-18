cf55347043e3c0aa4d9873b8113b7ab919d17cef
Silver <silver1867@outlook.com>
2024-09-18 19:28:43 +0800

    update

--- source/Controller/CCSOLUtility.cpp
+++ source/Controller/CCSOLUtility.cpp
@@ -2,6 +2,10 @@
 
 static CCSOLUtilitiy* m_singleton = nullptr;
 
+void CCSOLUtilitiy::InitializeInstance(std::string sGameRootPath, std::string sGameLauncherPath)
+{
+}
+
 CCSOLUtilitiy::CCSOLUtilitiy(std::string sGameRootPath, std::string sGameLaucherPath) :
 m_sGameRootPath(sGameRootPath), m_sGameLauncherPath(sGameLaucherPath)
 {
